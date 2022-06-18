3e80f5f846e7bfcc80140ca8197e8d92b3122885
2z0h0m9 <zhmzhm2021@outlook.com>
2022-06-18 21:20:10 +0800

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(name='pyToolBox',
       version='0.2',
       description='some useful tools of python',
-      url='http://github.com/2z0h0m9/pyUsefulTools',
+      url='http://github.com/2z0h0m9/pyToolBox',
       author='Ling',
       author_email='2z0h0m9@gmail.com',
       license='MIT',
