8a61a6997ae7f1520b31ea9a637c416e124775cb
2z0h0m9 <80618532+2z0h0m9@users.noreply.github.com>
2022-06-18 20:54:17 +0800

    Update init.py

--- pyToolBox/init.py
+++ pyToolBox/init.py
@@ -6,11 +6,9 @@ def self_reported():
         you can use this warehouse to do:
             1.encryption and decryption Base64 code
             2.elevate privileges(applying administrator rights on windows, macOS or linux)
-            3.make gif files
         这个库可以:
             1.加解密Base64
             2.提权(可以在windows, macOS和linux上使用)
-            3.制作GIF文件
         ''')
 
 
